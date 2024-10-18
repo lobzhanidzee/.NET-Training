1e16696dddb7318726fcbd6c63d68490e37c8b65
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-10-18 15:20:31 +0400

    student grade calculate foreach

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03159c5790344313b06083004a31f7ea3080e31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bde3d1cbb7e239f105c46a99a82438ffbd9572ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
