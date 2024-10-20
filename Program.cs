86120b8aadd035907622cbfbb7fb8ebe9a752198
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-10-20 14:10:50 +0400

    conditional operator

--- Program.cs
+++ Program.cs
@@ -1,141 +1,81 @@
-﻿/* 
-This C# console application is designed to:
-- Use arrays to store student names and assignment scores.
-- Use a `foreach` statement to iterate through the student names as an outer program loop.
-- Use an `if` statement within the outer loop to identify the current student name and access that student's assignment scores.
-- Use a `foreach` statement within the outer loop to iterate though the assignment scores array and sum the values.
-- Use an algorithm within the outer loop to calculate the average exam score for each student.
-- Use an `if-elseif-else` construct within the outer loop to evaluate the average exam score and assign a letter grade automatically.
-- Integrate extra credit scores when calculating the student's final score and letter grade as follows:
-    - detects extra credit assignments based on the number of elements in the student's scores array.
-    - divides the values of extra credit assignments by 10 before adding extra credit scores to the sum of exam scores.
-- use the following report format to report student grades: 
+﻿Console.Clear();
+// Console.WriteLine("a" == "a");
+// Console.WriteLine("a" == "A");
+// Console.WriteLine(1 == 2);
 
-    Student         Grade
+// string myValue = "a";
+// Console.WriteLine(myValue == "a");
 
-    Sophia:         92.2    A-
-    Andrew:         89.6    B+
-    Emma:           85.6    B
-    Logan:          91.2    A-
-*/
-int examAssignments = 5;
+// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
 
-string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
+// Console.WriteLine("a" != "a");
+// Console.WriteLine("a" != "A");
+// Console.WriteLine(1 != 2);
 
-int[] sophiaScores = [90, 86, 87, 98, 100, 94, 90];
-int[] andrewScores = [92, 89, 81, 96, 90, 89];
-int[] emmaScores = [90, 85, 87, 98, 68, 89, 89, 89];
-int[] loganScores = [90, 95, 87, 88, 96, 96];
+// string myValue = "a";
+// Console.WriteLine(myValue != "a");
 
-int[] studentScores = new int[10];
+// Console.WriteLine(1 > 2);
+// Console.WriteLine(1 < 2);
+// Console.WriteLine(1 >= 1);
+// Console.WriteLine(1 <= 1);
 
-string currentStudentLetterGrade = "";
+// string pangram = "The quick brown fox jumps over the lazy dog.";
 
-// display the header row for scores/grades
-Console.Clear();
-Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
 
-/*
-The outer foreach loop is used to:
-- iterate through student names 
-- assign a student's grades to the studentScores array
-- sum assignment scores (inner foreach loop)
-- calculate numeric and letter grade
-- write the score report information
-*/
-foreach (string name in studentNames)
+// These two lines of code will create the same output
+
+// Console.WriteLine(pangram.Contains("fox") == false);
+// Console.WriteLine(!pangram.Contains("fox"));
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(!pangram.Contains("fox"));
+// Console.WriteLine(!pangram.Contains("cow"));
+
+// int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+// Random coin = new Random();
+// int flip = coin.Next(1, 3);
+// string result = flip == 1 ? "Heads" : "Tails";
+// Console.WriteLine((result));
+
+string permission = "Admin";
+int level = 60;
+
+if (permission.Contains("Admin"))
 {
-    string currentStudent = name;
-
-    if (currentStudent == "Sophia")
-        studentScores = sophiaScores;
-
-    else if (currentStudent == "Andrew")
-        studentScores = andrewScores;
-
-    else if (currentStudent == "Emma")
-        studentScores = emmaScores;
-
-    else if (currentStudent == "Logan")
-        studentScores = loganScores;
-
-    int gradedAssignments = 0;
-    int gradedExtraCreditAssignments = 0;
-
-    int sumExamScores = 0;
-    int sumExtraCreditScores = 0;
-
-    decimal currentStudentGrade = 0;
-    decimal currentStudentExamScore = 0;
-    decimal currentStudentExtraCreditScore = 0;
-
-    /* 
-    the inner foreach loop: 
-    - sums the exam and extra credit scores
-    - counts the extra credit assignments
-    */
-    foreach (int score in studentScores)
+    if (level > 55)
     {
-        gradedAssignments++;
-
-        if (gradedAssignments <= examAssignments)
-        {
-            sumExamScores += score;
-        }
-
-        else
-        {
-            gradedExtraCreditAssignments++;
-            sumExtraCreditScores += score;
-        }
+        Console.WriteLine("Welcome, Super Admin user.");
     }
-
-    currentStudentExamScore = (decimal)(sumExamScores) / examAssignments;
-    currentStudentExtraCreditScore = (decimal)(sumExtraCreditScores) / gradedExtraCreditAssignments;
-
-    currentStudentGrade = (decimal)((decimal)sumExamScores + ((decimal)sumExtraCreditScores / 10)) / examAssignments;
-
-    if (currentStudentGrade >= 97)
-        currentStudentLetterGrade = "A+";
-
-    else if (currentStudentGrade >= 93)
-        currentStudentLetterGrade = "A";
-
-    else if (currentStudentGrade >= 90)
-        currentStudentLetterGrade = "A-";
-
-    else if (currentStudentGrade >= 87)
-        currentStudentLetterGrade = "B+";
-
-    else if (currentStudentGrade >= 83)
-        currentStudentLetterGrade = "B";
-
-    else if (currentStudentGrade >= 80)
-        currentStudentLetterGrade = "B-";
-
-    else if (currentStudentGrade >= 77)
-        currentStudentLetterGrade = "C+";
-
-    else if (currentStudentGrade >= 73)
-        currentStudentLetterGrade = "C";
-
-    else if (currentStudentGrade >= 70)
-        currentStudentLetterGrade = "C-";
-
-    else if (currentStudentGrade >= 67)
-        currentStudentLetterGrade = "D+";
-
-    else if (currentStudentGrade >= 63)
-        currentStudentLetterGrade = "D";
-
-    else if (currentStudentGrade >= 60)
-        currentStudentLetterGrade = "D-";
-
     else
-        currentStudentLetterGrade = "F";
-
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentExamScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{currentStudentExtraCreditScore} ({(((decimal)sumExtraCreditScores / 10) / examAssignments)} pts)");
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
 }
 
-Console.WriteLine("\n\rPress the Enter key to continue");
-Console.ReadLine();
+Console.WriteLine(permission.Contains("Admin") ? (level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user.") :
+ (permission.Contains("Manager") ? (level >= 20 ? "Contact an Admin for access." : "You do not have sufficient privileges.") :
+  "You do not have sufficient privileges."));
+
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e16696dddb7318726fcbd6c63d68490e37c8b65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca62af16886ccdfd446f5bda2d27ae2e76d9a01a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
