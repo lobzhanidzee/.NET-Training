b4e5ae92f37a269853aa0c601396f26a6254ce95
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-10-22 16:08:29 +0400

    switch case

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86120b8aadd035907622cbfbb7fb8ebe9a752198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db03cc809d514961a0e89b4a39560f3806a660d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
