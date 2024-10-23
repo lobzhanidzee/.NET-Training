6e8a0daf69108178638fee70b6edf94e2282229a
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-10-23 23:56:00 +0400

    for loop

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db03cc809d514961a0e89b4a39560f3806a660d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e5ae92f37a269853aa0c601396f26a6254ce95")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
