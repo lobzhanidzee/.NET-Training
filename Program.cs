d77ff79b147c5ae94cec6aa0147a471153561fe6
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-11-21 20:11:39 +0400

    array helper methods

--- Program.cs
+++ Program.cs
@@ -1,24 +1,119 @@
-﻿Console.Clear();
-// int first = 2;
-// string second = "4";
-// string result = first + second;
+﻿// string[] pallets = ["B14", "A11", "B12", "A13"];
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = ["B14", "A11", "B12", "A13"];
+// Console.WriteLine("");
+
+// Console.WriteLine($"Before: {pallets[0].ToLower()}");
+// Array.Clear(pallets, 0, 2);
+
+// if (pallets[0] != null)
+//     Console.WriteLine($"After: {pallets[0].ToLower()}");
+
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = ["B14", "A11", "B12", "A13"];
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 6);
+// Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+// pallets[4] = "C01";
+// pallets[5] = "C02";
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Array.Resize(ref pallets, 3);
+// Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+
 // Console.WriteLine(result);
 
-// int myInt = 3;
-// Console.WriteLine($"int: {myInt}");
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
 
-// decimal myDecimal = myInt;
-// Console.WriteLine($"decimal: {myDecimal}");
+// string pangram = "The quick brown fox jumps over the lazy dog";
 
-// decimal myDecimal = 3.14m;
-// Console.WriteLine($"decimal: {myDecimal}");
+// // Step 1
+// string[] message = pangram.Split(' ');
 
-// int myInt = (int)myDecimal;
-// Console.WriteLine($"decimal: {myInt}");
+// //Step 2
+// string[] newMessage = new string[message.Length];
 
+// // Step 3
+// for (int i = 0; i < message.Length; i++)
+// {
+//     char[] letters = message[i].ToCharArray();
+//     Array.Reverse(letters);
+//     newMessage[i] = new string(letters);
+// }
 
-decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
+// //Step 4
+// string result = String.Join(" ", newMessage);
+// Console.WriteLine(result);
 
-Console.WriteLine($"decimal: {myDecimal}");
-Console.WriteLine($"float:\t {myFloat}");
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderArr = orderStream.Split(",");
+Array.Sort(orderArr);
+
+foreach (string item in orderArr)
+{
+    if (item.Length != 4)
+        Console.WriteLine($"{item}\t- Error");
+    else
+        Console.WriteLine(item);
+}
+
+// for (int i = 0; i < orderArr.Length; i++)
+// {
+//     if (orderArr[i].Length != 4)
+//     {
+//         orderArr[i] = $"{orderArr[i]}\t- Error";
+//     }
+//     Console.WriteLine(orderArr[i]);
+// }
+
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f114f4d254ae1583175aced37f2ec8f16811ad20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54dfb593fcf2fa1294a176f509aa18aa8877a061")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
