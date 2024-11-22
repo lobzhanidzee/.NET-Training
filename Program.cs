7a91d640e4a48cc294bc20fffbab3e4ebf8b3bfa
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-11-23 00:50:33 +0400

    string helper methods 1

--- Program.cs
+++ Program.cs
@@ -1,111 +1,40 @@
 ﻿Console.Clear();
+// string message = "Find what is (inside the parentheses)";
 
-// string first = "Hello";
-// string second = "World";
-// string result = string.Format("{0} {1}!", first, second);
-// Console.WriteLine(result);
+// int openingPosition = message.IndexOf('(');
+// int closingPosition = message.IndexOf(')');
 
-// string first = "Hello";
-// string second = "World";
-// Console.WriteLine("{1} {0}!", first, second);
-// Console.WriteLine("{0} {0} {0}!", first, second);
+// // Console.WriteLine(openingPosition);
+// // Console.WriteLine(closingPosition);
 
-// string first = "Hello";
-// string second = "World";
-// Console.WriteLine($"{first} {second}!");
-// Console.WriteLine($"{second} {first}!");
-// Console.WriteLine($"{first} {first} {first}!");
+// openingPosition += 1;
 
-// decimal price = 123.45m;
-// int discount = 50;
-// Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
 
-// decimal measurement = 123456.78912m;
-// Console.WriteLine($"Measurement: {measurement:N5} units");
+// string message = "What is the value <span>between the tags</span>?";
 
-// decimal tax = .36785m;
-// Console.WriteLine($"Tax rate: {tax:P2}");
+// const string openSpan = "<span>";
+// const string closeSpan = "</span>";
 
-// decimal price = 67.55m;
-// decimal salePrice = 59.99m;
+// int openingPosition = message.IndexOf(openSpan);
+// int closingPosition = message.IndexOf(closeSpan);
 
-// string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+// openingPosition += openSpan.Length;
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
 
-// yourDiscount += $"A discount of {((price - salePrice) / price):P2}!";
-// Console.WriteLine(yourDiscount);
+// string message = "hello there!";
 
-// int invoiceNumber = 1201;
-// decimal productShares = 25.4568m;
-// decimal subtotal = 2750.00m;
-// decimal taxPercentage = .15825m;
-// decimal total = 3185.19m;
+// int first_h = message.IndexOf('h');
+// int last_h = message.LastIndexOf('h');
 
-// Console.WriteLine($"Invoice Number: {invoiceNumber}");
-// Console.WriteLine($"    Shares: {productShares:N3} Product");
-// Console.WriteLine($"        Sub Total: {subtotal:C}");
-// Console.WriteLine($"            Tax: {taxPercentage:P2}");
-// Console.WriteLine($"        Total Billed: {total:C}");
+// Console.WriteLine($"For the message : '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}");
 
-// string input = "Pad this";
-// Console.WriteLine(input.PadLeft(12));
-// Console.WriteLine(input.PadRight(12));
+string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition = message.LastIndexOf('(');
 
-// Console.WriteLine(input.PadLeft(12, '-'));
-// Console.WriteLine(input.PadRight(12, '-'));
-
-// string paymentId = "769C";
-// string payeeName = "Mr. Stephen Ortega";
-// string payementAmount = "$5,000.00";
-
-// var formattedLine = paymentId.PadRight(6);
-// formattedLine += payeeName.PadRight(24);
-// formattedLine += payementAmount.PadLeft(10);
-
-// Console.WriteLine("1234567890123456789012345678901234567890");
-// Console.WriteLine(formattedLine);
-
-
-string customerName = "Ms. Barros";
-
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
-
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
-
-// Your logic here
-Console.WriteLine($"Dear {customerName},");
-Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
-Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
-Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
-
-Console.WriteLine("Here's a quick comparison:\n");
-
-
-string comparisonMessage = "";
-
-// Your logic here
-comparisonMessage = currentProduct.PadRight(20);
-comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
-comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
-
-comparisonMessage += "\n";
-comparisonMessage += newProduct.PadRight(20);
-comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
-comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
-
-// comparisonMessage += $"{currentProduct.PadRight(20)}";
-// comparisonMessage += $"{currentReturn:P2}";
-// comparisonMessage += $"{currentProfit.ToString("C").PadLeft(19)}\n";
-
-
-// comparisonMessage += $"{newProduct.PadRight(20)}";
-// comparisonMessage += $"{newReturn:P2}";
-// comparisonMessage += $"{newProfit.ToString("C").PadLeft(19)}";
-
-
-
-Console.WriteLine(comparisonMessage);
+openingPosition += 1;
+int closingPosition = message.LastIndexOf(')');
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d77ff79b147c5ae94cec6aa0147a471153561fe6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b411f73d71c04663690f0ea56ea1cca3648f3120")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
