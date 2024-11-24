2d5d315c825420bab83eabcc14870db4c74a5a69
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-11-24 18:57:41 +0400

    string helper methods 2

--- Program.cs
+++ Program.cs
@@ -31,10 +31,108 @@
 
 // Console.WriteLine($"For the message : '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}");
 
-string message = "(What if) I am (only interested) in the last (set of parentheses)?";
-int openingPosition = message.LastIndexOf('(');
+// string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+// int openingPosition = message.LastIndexOf('(');
 
-openingPosition += 1;
-int closingPosition = message.LastIndexOf(')');
-int length = closingPosition - openingPosition;
-Console.WriteLine(message.Substring(openingPosition, length));
+// openingPosition += 1;
+// int closingPosition = message.LastIndexOf(')');
+// int length = closingPosition - openingPosition;
+// Console.WriteLine(message.Substring(openingPosition, length));
+
+// string message = "(What if) there are (more than) one (set of parentheses)?";
+// while (true)
+// {
+//     int openingPosition = message.IndexOf('(');
+//     if (openingPosition == -1) break;
+
+//     openingPosition += 1;
+//     int closingPosition = message.IndexOf(')');
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+
+//     message = message.Substring(closingPosition + 1);
+// }
+
+
+// string message = "Help (find) the {opening symbols}";
+// Console.WriteLine($"Searching THIS Message: {message}");
+
+// char[] openSymbols = { '[', '{', '(' };
+// int startPosition = 5;
+// int openingPosition = message.IndexOfAny(openSymbols);
+// Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
+
+// openingPosition = message.IndexOfAny(openSymbols, startPosition);
+// Console.WriteLine($"Found WITH using startPosotion {startPosition}:  {message.Substring(openingPosition)}");
+
+
+
+// string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// char[] openSymbols = { '[', '{', '(' };
+
+// int closingPosition = 0;
+
+// while (true)
+// {
+//     int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+//     if (openingPosition == -1) break;
+
+//     string currentSymbol = message.Substring(openingPosition, 1);
+
+//     char matchingSymbol = ' ';
+
+//     switch (currentSymbol)
+//     {
+
+//         case "[":
+//             matchingSymbol = ']';
+//             break;
+//         case "{":
+//             matchingSymbol = '}';
+//             break;
+//         case "(":
+//             matchingSymbol = ')';
+//             break;
+//     }
+
+//     openingPosition += 1;
+//     closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+//     int length = closingPosition - openingPosition;
+//     Console.WriteLine(message.Substring(openingPosition, length));
+// }
+
+
+// string data = "12345John Smith          5000  3  ";
+
+// string updatedData = data.Remove(5, 20);
+// Console.WriteLine(updatedData);
+
+
+// string message = "This--is--ex-amp-le--da-ta";
+// message = message.Replace("--", " ").Replace("-", "");
+// Console.WriteLine(message);
+
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+int spanIndex = input.IndexOf("<span>") + 6;
+int spanEndIndex = input.IndexOf("</span>");
+int length = spanEndIndex - spanIndex;
+
+quantity = input.Substring(spanIndex, length);
+
+int divIndex = input.IndexOf("<div>") + 5;
+int divEndIndex = input.IndexOf("</div>");
+length = divEndIndex - divIndex;
+
+output = input.Substring(divIndex, length).Replace("&trade", "&reg");
+
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b411f73d71c04663690f0ea56ea1cca3648f3120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a91d640e4a48cc294bc20fffbab3e4ebf8b3bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
