858a71f04754c1a3a2e73f81b9dc409ac82037a7
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-11-30 16:49:53 +0400

    C# methods (default value)

--- Program.cs
+++ Program.cs
@@ -1,180 +1,174 @@
-﻿// int[] a = { 1, 2, 3, 4, 5 };
+﻿using System.Net.NetworkInformation;
 
-// Console.WriteLine("Contents of Array:");
-// PrintArray();
+Console.Clear();
 
-// void PrintArray()
+// CountTo(5);
+
+// void CountTo(int max)
 // {
-//     foreach (int x in a)
+//     for (int i = 0; i < max; i++)
 //     {
-//         Console.Write($"{x} ");
-//     }
-//     Console.WriteLine();
-// }
-
-// Console.WriteLine("Before calling a method");
-// SayHello();
-// Console.WriteLine("After calling a method");
-
-// void SayHello()
-// {
-//     Console.WriteLine("Hello World!");
-// }
-
-// Console.WriteLine("Generating random numbers:");
-// DisplayRandomNumbers();
-
-// void DisplayRandomNumbers()
-// {
-//     Random random = new Random();
-
-//     for (int i = 0; i < 5; i++)
-//     {
-//         Console.Write($"{random.Next(1, 100)} ");
-//     }
-
-//     Console.WriteLine();
-// }
-
-
-// int[] times = { 800, 1200, 1600, 2000 };
-// int diff = 0;
-
-// Console.WriteLine("Enter current GMT");
-// int currentGMT = Convert.ToInt32(Console.ReadLine());
-
-// Console.WriteLine("Current Medicine Schedule:");
-// DisplayTimes();
-
-// Console.WriteLine("Enter new GMT");
-// int newGMT = Convert.ToInt32(Console.ReadLine());
-
-// if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
-// {
-//     Console.WriteLine("Invalid GMT");
-// }
-// else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-// {
-//     diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
-//     AdjustTimes();
-// }
-// else
-// {
-//     diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
-//     AdjustTimes();
-// }
-
-// Console.WriteLine("New Medicine Schedule:");
-// DisplayTimes();
-
-// void DisplayTimes()
-// {
-//     /* Format and display medicine times */
-//     foreach (int val in times)
-//     {
-//         string time = val.ToString();
-//         int len = time.Length;
-
-//         if (len >= 3)
-//         {
-//             time = time.Insert(len - 2, ":");
-//         }
-//         else if (len == 2)
-//         {
-//             time = time.Insert(0, "0:");
-//         }
-//         else
-//         {
-//             time = time.Insert(0, "0:0");
-//         }
-
-//         Console.Write($"{time} ");
-//     }
-//     Console.WriteLine();
-// }
-
-// void AdjustTimes()
-// {
-//     /* Adjust the times by adding the difference, keeping the value within 24 hours */
-//     for (int i = 0; i < times.Length; i++)
-//     {
-//         times[i] = ((times[i] + diff)) % 2400;
+//         Console.Write($"{i}, ");
 //     }
 // }
 
-// string[] ipv4Input = { "107.31.1.5", "255.0.0.255", "555..0.555", "255...255" };
-// string[] address;
+// int[] schedule = { 800, 1200, 1600, 2000 };
 
-// bool validLength = false;
-// bool validZeroes = false;
-// bool validRange = false;
+// DisplayAdjustedTimes(schedule, 6, -6);
 
-// foreach (string ip in ipv4Input)
+
+// void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT)
 // {
-//     address = ip.Split('.', StringSplitOptions.RemoveEmptyEntries);
-
-//     ValidateLength();
-//     ValidateZeroes();
-//     ValidateRange();
-
-//     if (validLength && validZeroes && validRange)
+//     int diff = 0;
+//     if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
 //     {
-//         Console.WriteLine($"{ip} is a valid IPv4 address");
+//         Console.WriteLine("Invalid GMT");
+//     }
+//     else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+//     {
+//         diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
 //     }
 //     else
 //     {
-//         Console.WriteLine($"{ip} is an invalid IPv4 address");
+//         diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
 //     }
-// }
 
-// void ValidateLength()
-// {
-//     validLength = address.Length == 4;
-// }
-// void ValidateZeroes()
-// {
-
-//     foreach (string number in address)
+//     for (int i = 0; i < times.Length; i++)
 //     {
-//         if (number.Length > 1 && number.StartsWith("0"))
-//         {
-//             validZeroes = false;
-//             return;
-//         }
+//         int newTime = ((times[i] + diff)) % 2400;
+//         Console.WriteLine($"{times[i]} -> {newTime}");
 //     }
-//     validZeroes = true;
 // }
-// void ValidateRange()
+
+// string[] students = { "Jenna", "Ayesha", "Carlos", "Viktor" };
+
+// DisplayStudents(students);
+// DisplayStudents(new string[] { "Robert", "David" });
+
+// void DisplayStudents(string[] students)
 // {
-
-//     foreach (string number in address)
+//     foreach (string student in students)
 //     {
-//         int value = int.Parse(number);
-//         if (value < 0 || value > 255)
-//         {
-//             validRange = false;
-//             return;
-//         }
+//         Console.Write($"{student}, ");
 //     }
-//     validRange = true;
+//     Console.WriteLine();
 // }
 
-Random random = new Random();
-int luck = random.Next(100);
 
-string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
-string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
-string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
-string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
+// double pi = 3.14159;
 
-TellFortune();
+// PrintCircleArea(12);
+// PrintCircleCirumference(12);
+// PrintCircleInfo(12);
 
-void TellFortune()
+// void PrintCircleInfo(int radius)
+// {
+//     Console.WriteLine($"Circle with radius {radius}");
+//     PrintCircleArea(radius);
+//     PrintCircleCirumference(radius);
+// }
+
+// void PrintCircleArea(int radius)
+// {
+//     double area = pi * (radius * radius);
+//     Console.WriteLine($"Area = {area}");
+// }
+
+// void PrintCircleCirumference(int radius)
+// {
+//     double circumference = 2 * pi * radius;
+//     Console.WriteLine($"Circumference = {circumference}");
+// }
+
+// int a = 3;
+// int b = 4;
+// int c = 0;
+
+// Multiply(a, b, c);
+// Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+// void Multiply(int a, int b, int c)
+// {
+//     c = a * b;
+//     Console.WriteLine($"inside Multiply method: {a} * {b} = {c}");
+// }
+
+// int[] array = { 1, 2, 3, 4, 5 };
+
+// PrintArray(array);
+// Clear(array);
+// PrintArray(array);
+
+// void PrintArray(int[] array)
+// {
+//     foreach (int a in array)
+//     {
+//         Console.Write($"{a} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void Clear(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = 0;
+//     }
+// }
+
+// string status = "Healthy";
+
+// Console.WriteLine($"Start: {status}");
+// SetHealth(false);
+// Console.WriteLine($"End: {status}");
+
+// void SetHealth(bool isHealthy)
+// {
+//     status = (isHealthy ? "Healthy" : "Unhealthy");
+//     Console.WriteLine($"Middle: {status}");
+// }
+
+string[] guestList = { "Rebecca", "Nadia", "Noor", "Jonte" };
+string[] rsvps = new string[10];
+int count = 0;
+
+RSVP("Rebecca");
+RSVP("Nadia", 2, "Nuts");
+RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+RSVP("Noor", 4, inviteOnly: false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
+
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
 {
-    Console.WriteLine("A fortune teller whispers the following words:");
-    string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
-    for (int i = 0; i < 4; i++)
+    if (inviteOnly)
     {
-        Console.Write($"{text[i]} {fortune[i]} ");
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name))
+            {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies; {allergies}";
+    count++;
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvps[i]);
     }
 }
