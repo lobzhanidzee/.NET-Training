672764a0cf1d00e9f834dd113df883c2bcb84b42
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2024-12-12 17:16:42 +0400

    Guided Project - Debug and Handle eceptions

--- Program.cs
+++ Program.cs
@@ -1,270 +1,176 @@
-﻿Console.Clear();
+﻿/*
+This application manages transactions at a store check-out line. The
+check-out line has a cash register, and the register has a cash till
+that is prepared with a number of bills each morning. The till includes
+bills of four denominations: $1, $5, $10, and $20. The till is used
+to provide the customer with change during the transaction. The item 
+cost is a randomly generated number between 2 and 49. The customer 
+offers payment based on an algorithm that determines a number of bills
+in each denomination. 
 
-// ArgumentException invalidArgumentException = new ArgumentException("ArgumentException: The 'GraphData' method received data outside the expected range.");
-// throw invalidArgumentException;
+Each day, the cash till is loaded at the start of the day. As transactions
+occur, the cash till is managed in a method named MakeChange (customer 
+payments go in and the change returned to the customer comes out). A 
+separate "safety check" calculation that's used to verify the amount of
+money in the till is performed in the "main program". This safety check
+is used to ensure that logic in the MakeChange method is working as 
+expected.
+*/
 
-// throw new FormatException("FormatException: Calculations in process XYZ have been cancelled due to invalid data format.");
+string? readResult = null;
+bool useTestData = false;
 
-// string[][] userEnteredValues = new string[][]
-// {
-//         new string[] { "1", "two", "3"},
-//         new string[] { "0", "1", "2"}
-// };
+Console.Clear();
 
-// foreach (string[] userEntries in userEnteredValues)
-// {
-//     try
-//     {
-//         BusinessProcess1(userEntries);
-//     }
-//     catch (Exception ex)
-//     {
-//         if (ex.StackTrace.Contains("BusinessProcess1") && (ex is FormatException))
-//         {
-//             Console.WriteLine(ex.Message);
-//         }
-//     }
-// }
+int[] cashTill = new int[] { 0, 0, 0, 0 };
+int registerCheckTillTotal = 0;
 
-// static void BusinessProcess1(string[] userEntries)
-// {
-//     int valueEntered;
+// registerDailyStartingCash: $1 x 50, $5 x 20, $10 x 10, $20 x 5 => ($350 total)
+int[,] registerDailyStartingCash = new int[,] { { 1, 50 }, { 5, 20 }, { 10, 10 }, { 20, 5 } };
 
-//     foreach (string userValue in userEntries)
-//     {
-//         try
-//         {
-//             valueEntered = int.Parse(userValue);
+int[] testData = new int[] { 6, 10, 17, 20, 31, 36, 40, 41 };
+int testCounter = 0;
 
-//             // completes required calculations based on userValue
-//             // ...
-//         }
-//         catch (FormatException)
-//         {
-//             FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
-//             throw invalidFormatException;
-//         }
-//     }
-// }
+LoadTillEachMorning(registerDailyStartingCash, cashTill);
 
-// try
-// {
-//     OperatingProcedure1();
-// }
-// catch (Exception ex)
-// {
-//     Console.WriteLine(ex.Message);
-//     Console.WriteLine("Exiting application.");
-// }
+registerCheckTillTotal = registerDailyStartingCash[0, 0] * registerDailyStartingCash[0, 1] + registerDailyStartingCash[1, 0] * registerDailyStartingCash[1, 1] + registerDailyStartingCash[2, 0] * registerDailyStartingCash[2, 1] + registerDailyStartingCash[3, 0] * registerDailyStartingCash[3, 1];
 
-// static void OperatingProcedure1()
-// {
-//     string[][] userEnteredValues = new string[][]
-//     {
-//         new string[] { "1", "two", "3"},
-//         new string[] { "0", "1", "2"}
-//     };
+// display the number of bills of each denomination currently in the till
+LogTillStatus(cashTill);
 
-//     foreach (string[] userEntries in userEnteredValues)
-//     {
-//         try
-//         {
-//             BusinessProcess1(userEntries);
-//         }
-//         catch (Exception ex)
-//         {
-//             if (ex.StackTrace.Contains("BusinessProcess1"))
-//             {
-//                 if (ex is FormatException)
-//                 {
-//                     Console.WriteLine(ex.Message);
-//                     Console.WriteLine("Corrective action taken in OperatingProcedure1");
-//                 }
-//                 else if (ex is DivideByZeroException)
-//                 {
-//                     Console.WriteLine(ex.Message);
-//                     Console.WriteLine("Partial correction in OperatingProcedure1 - further action required");
+// display a message showing the amount of cash in the till
+Console.WriteLine(TillAmountSummary(cashTill));
 
-//                     // re-throw the original exception
-//                     throw;
-//                 }
-//                 else
-//                 {
-//                     // create a new exception object that wraps the original exception
-//                     throw new ApplicationException("An error occurred - ", ex);
-//                 }
-//             }
-//         }
+// display the expected registerDailyStartingCash total
+Console.WriteLine($"Expected till value: {registerCheckTillTotal}\n\r");
 
-//     }
-// }
+var valueGenerator = new Random((int)DateTime.Now.Ticks);
 
-// static void BusinessProcess1(string[] userEntries)
-// {
-//     int valueEntered;
+int transactions = 10;
 
-//     foreach (string userValue in userEntries)
-//     {
-//         try
-//         {
-//             valueEntered = int.Parse(userValue);
-
-//             checked
-//             {
-//                 int calculatedValue = 4 / valueEntered;
-//             }
-//         }
-//         catch (FormatException)
-//         {
-//             FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
-//             throw invalidFormatException;
-//         }
-//         catch (DivideByZeroException)
-//         {
-//             DivideByZeroException unexpectedDivideByZeroException = new DivideByZeroException("DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero");
-//             throw unexpectedDivideByZeroException;
-
-//         }
-//     }
-// }
-
-// Prompt the user for the lower and upper bounds
-// Console.Write("Enter the lower bound: ");
-// int lowerBound = int.Parse(Console.ReadLine());
-
-// Console.Write("Enter the upper bound: ");
-// int upperBound = int.Parse(Console.ReadLine());
-
-// decimal averageValue = 0;
-// bool exit = false;
-
-// do
-// {
-
-//     try
-//     {
-//         // Calculate the sum of the even numbers between the bounds
-//         averageValue = AverageOfEvenNumbers(lowerBound, upperBound);
-
-//         // Display the value returned by AverageOfEvenNumbers in the console
-//         Console.WriteLine($"The average of even numbers between {lowerBound} and {upperBound} is {averageValue}.");
-
-//         exit = true;
-//     }
-//     catch (ArgumentOutOfRangeException ex)
-//     {
-//         Console.WriteLine("An error has occurred.");
-//         Console.WriteLine(ex.Message);
-//         Console.WriteLine($"The upper bound must be greater than {lowerBound}");
-//         Console.Write($"Enter a new upper bound (or enter Exit to quit): ");
-//         string? userResponse = Console.ReadLine();
-//         if (userResponse.ToLower().Contains("exit"))
-//         {
-//             exit = true;
-//         }
-//         else
-//         {
-//             exit = false;
-//             upperBound = int.Parse(Console.ReadLine());
-//         }
-//     }
-// } while (exit == false);
-
-
-// // Wait for user input
-// Console.ReadLine();
-
-// static decimal AverageOfEvenNumbers(int lowerBound, int upperBound)
-// {
-//     if (lowerBound >= upperBound)
-//     {
-//         throw new ArgumentOutOfRangeException("upperBound", "ArgumentOutOfRangeException: upper bound must be greater than lower bound.");
-//     }
-
-//     int sum = 0;
-//     int count = 0;
-//     decimal average = 0m;
-
-
-//     for (int i = lowerBound; i <= upperBound; i++)
-//     {
-//         if (i % 2 == 0)
-//         {
-//             sum += i;
-//             count++;
-//         }
-//     }
-
-//     average = (decimal)sum / count;
-
-//     return average;
-// }
-
-string[][] userEnteredValues = new string[][]
+if (useTestData)
 {
-            new string[] { "1", "2", "3"},
-            new string[] { "1", "two", "3"},
-            new string[] { "0", "1", "2"}
-};
-
-try
-{
-    Workflow1(userEnteredValues);
-    Console.WriteLine("'Workflow1' completed successfully.");
+    transactions = testData.Length;
 }
-catch (DivideByZeroException ex)
+
+while (transactions > 0)
 {
-    Console.WriteLine("An error occurred during 'Workflow1'.");
-    Console.WriteLine(ex.Message);
+    transactions -= 1;
+    int itemCost = valueGenerator.Next(2, 20);
+
+    if (useTestData)
+    {
+        itemCost = testData[testCounter];
+        testCounter += 1;
+    }
+
+    int paymentOnes = itemCost % 2;                 // value is 1 when itemCost is odd, value is 0 when itemCost is even
+    int paymentFives = (itemCost % 10 > 7) ? 1 : 0; // value is 1 when itemCost ends with 8 or 9, otherwise value is 0
+    int paymentTens = (itemCost % 20 > 13) ? 1 : 0; // value is 1 when 13 < itemCost < 20 OR 33 < itemCost < 40, otherwise value is 0
+    int paymentTwenties = (itemCost < 20) ? 1 : 2;  // value is 1 when itemCost < 20, otherwise value is 2
+
+    // display messages describing the current transaction
+    Console.WriteLine($"Customer is making a ${itemCost} purchase");
+    Console.WriteLine($"\t Using {paymentTwenties} twenty dollar bills");
+    Console.WriteLine($"\t Using {paymentTens} ten dollar bills");
+    Console.WriteLine($"\t Using {paymentFives} five dollar bills");
+    Console.WriteLine($"\t Using {paymentOnes} one dollar bills");
+
+    try
+    {
+        // MakeChange manages the transaction and updates the till 
+        MakeChange(itemCost, cashTill, paymentTwenties, paymentTens, paymentFives, paymentOnes);
+
+        Console.WriteLine($"Transaction successfully completed.");
+        registerCheckTillTotal += itemCost;
+    }
+    catch (InvalidOperationException ex)
+    {
+        Console.WriteLine($"Could not complete transaction: {ex.Message}");
+    };
+
+    Console.WriteLine(TillAmountSummary(cashTill));
+    Console.WriteLine($"Expected till value: {registerCheckTillTotal}\n\r");
     Console.WriteLine();
 }
 
-
-static void Workflow1(string[][] userEnteredValues)
+Console.WriteLine("Press the Enter key to exit");
+do
 {
-    foreach (string[] userEntries in userEnteredValues)
-    {
-        try
-        {
-            Process1(userEntries);
-            Console.WriteLine("'Process1' completed successfully.");
-            Console.WriteLine();
-        }
-        catch (FormatException ex)
-        {
-            Console.WriteLine("'Process1' encountered an issue, process aborted.");
-            Console.WriteLine(ex.Message);
-            Console.WriteLine();
-        }
-    }
+    readResult = Console.ReadLine();
+
+} while (readResult == null);
+
+
+static void LoadTillEachMorning(int[,] registerDailyStartingCash, int[] cashTill)
+{
+    cashTill[0] = registerDailyStartingCash[0, 1];
+    cashTill[1] = registerDailyStartingCash[1, 1];
+    cashTill[2] = registerDailyStartingCash[2, 1];
+    cashTill[3] = registerDailyStartingCash[3, 1];
 }
 
-static void Process1(String[] userEntries)
+
+static void MakeChange(int cost, int[] cashTill, int twenties, int tens = 0, int fives = 0, int ones = 0)
 {
-    int valueEntered;
+    cashTill[3] += twenties;
+    cashTill[2] += tens;
+    cashTill[1] += fives;
+    cashTill[0] += ones;
 
-    foreach (string userValue in userEntries)
+    int amountPaid = twenties * 20 + tens * 10 + fives * 5 + ones;
+    int changeNeeded = amountPaid - cost;
+
+    if (changeNeeded < 0)
+        throw new InvalidOperationException("InvalidOperationException: Not enough money provided to complete the transaction.");
+
+
+    Console.WriteLine("Cashier Returns:");
+
+    while ((changeNeeded > 19) && (cashTill[3] > 0))
     {
-
-        bool integerFormat = int.TryParse(userValue, out valueEntered);
-
-        if (integerFormat == true)
-        {
-            if (valueEntered != 0)
-            {
-                checked
-                {
-                    int calculatedValue = 4 / valueEntered;
-                }
-            }
-            else
-            {
-                throw new DivideByZeroException("Invalid data. User input values must be non-zero values.");
-            }
-        }
-        else
-        {
-            throw new FormatException("Invalid data. User input values must be valid integers.");
-        }
+        cashTill[3]--;
+        changeNeeded -= 20;
+        Console.WriteLine("\t A twenty");
     }
+
+    while ((changeNeeded > 9) && (cashTill[2] > 0))
+    {
+        cashTill[2]--;
+        changeNeeded -= 10;
+        Console.WriteLine("\t A ten");
+    }
+
+    while ((changeNeeded > 4) && (cashTill[1] > 0))
+    {
+        cashTill[1]--;
+        changeNeeded -= 5;
+        Console.WriteLine("\t A five");
+    }
+
+    while ((changeNeeded > 0) && (cashTill[0] > 0))
+    {
+        cashTill[0]--;
+        changeNeeded--;
+        Console.WriteLine("\t A one");
+    }
+
+    if (changeNeeded > 0)
+        throw new InvalidOperationException("InvalidOperationException: The till is unable to make the correct change.");
+}
+
+static void LogTillStatus(int[] cashTill)
+{
+    Console.WriteLine("The till currently has:");
+    Console.WriteLine($"{cashTill[3] * 20} in twenties");
+    Console.WriteLine($"{cashTill[2] * 10} in tens");
+    Console.WriteLine($"{cashTill[1] * 5} in fives");
+    Console.WriteLine($"{cashTill[0]} in ones");
+    Console.WriteLine();
+}
+
+static string TillAmountSummary(int[] cashTill)
+{
+    return $"The till has {cashTill[3] * 20 + cashTill[2] * 10 + cashTill[1] * 5 + cashTill[0]} dollars";
+
 }
