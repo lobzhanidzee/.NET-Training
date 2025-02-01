30e01418e8e4fbc4bef6447c7b64244c1f500cc4
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2025-02-01 17:35:07 +0400

    Classes & Objects (static constructor)

--- UmlDesignBasics/CreatingSimpleClass16.cs
+++ UmlDesignBasics/CreatingSimpleClass16.cs
@@ -4,31 +4,26 @@ public static class CreatingSimpleClass16
 {
     public static object CreateSimpleClass16WithDefaultValues()
     {
-        // TODO: Implement this method.
-        throw new NotImplementedException();
+        return new SimpleClass16();
     }
 
     public static object CreateSimpleClass16(long longValue)
     {
-        // TODO: Implement this method.
-        throw new NotImplementedException();
+        return new SimpleClass16(longValue: longValue);
     }
 
     public static object CreateSimpleClass16(float floatValue)
     {
-        // TODO: Implement this method.
-        throw new NotImplementedException();
+        return new SimpleClass16(floatValue: floatValue);
     }
 
     public static object CreateSimpleClass16(int intValue, bool boolValue)
     {
-        // TODO: Implement this method.
-        throw new NotImplementedException();
+        return new SimpleClass16(intValue, booleanValue: boolValue);
     }
 
     public static object CreateSimpleClass16(double doubleValue, char charValue, string stringValue)
     {
-        // TODO: Implement this method.
-        throw new NotImplementedException();
+        return new SimpleClass16(doubleValue: doubleValue, charValue: charValue, stringValue: stringValue);
     }
 }
