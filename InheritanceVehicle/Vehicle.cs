a53818e99f642ca462577513d186f4951527ae4c
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2025-03-07 19:03:57 +0400

    inheritance 5

--- InheritanceVehicle/Vehicle.cs
+++ InheritanceVehicle/Vehicle.cs
@@ -5,8 +5,10 @@ public class Vehicle
     private string? name;
     private readonly int maxSpeed;
 
-    public Vehicle()
+    public Vehicle(string name, int maxSpeed)
     {
+        this.Name = name;
+        this.maxSpeed = maxSpeed;
     }
 
     public int MaxSpeed { get => this.maxSpeed; }
