0d5a11e89cc6a52d2f58c2908358d40c36486c6d
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2025-03-16 22:58:30 +0400

    employee-management-system manager class 1

--- WorkforceStructure/Employee.cs
+++ WorkforceStructure/Employee.cs
@@ -10,62 +10,57 @@ public class Employee
     private string name;
     private int salary;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Employee"/> class with the specified name and salary.
+    /// </summary>
+    /// <param name="name">The name of the employee.</param>
+    /// <param name="salary">The salary of the employee.</param>
+    /// <exception cref="ArgumentNullException">Thrown when name is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown salary less than 0.</exception>
+    // TODO: Implement the constructor logic
     public Employee(string name, int salary)
     {
         this.Name = name;
         this.Salary = salary;
     }
 
+    /// <summary>
+    /// Gets the name of the employee.
+    /// </summary>
+    // TODO: Implement the Name property
     public string Name { get => this.name; set => this.name = value; }
 
+    /// <summary>
+    /// Gets or sets the salary of the employee.
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown value less than 0.</exception>
+    // TODO: Implement the Salary property
     public int Salary { get => this.salary; set => this.salary = value; }
 
+
+    /// <summary>
+    /// Assigns a bonus to the employee.
+    /// </summary>
+    /// <param name="bonus">The bonus amount.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown bonus less than 0.</exception>
+    // TODO: Implement the AssignBonus method
     public void AssingBonus() { }
 
+
+    /// <summary>
+    /// Calculates the total amount to be paid to the employee, including bonuses.
+    /// </summary>
+    /// <returns>The total amount to be paid to the employee.</returns>
+    // TODO: Implement the CalculateTotalPay method
     public void CalculateTotalPay() { }
 
+    /// <summary>
+    /// Returns a string representation of the employee.
+    /// </summary>
+    /// <returns>A string that represents the employee.</returns>
+    // TODO: Implement the ToString method
     public override string ToString()
     {
         return $"{this.Name}, Salary: {this.Salary}";
     }
 }
-
-
-/// <summary>
-/// Initializes a new instance of the <see cref="Employee"/> class with the specified name and salary.
-/// </summary>
-/// <param name="name">The name of the employee.</param>
-/// <param name="salary">The salary of the employee.</param>
-/// <exception cref="ArgumentNullException">Thrown when name is null.</exception>
-/// <exception cref="ArgumentOutOfRangeException">Thrown salary less than 0.</exception>
-// TODO: Implement the constructor logic
-
-/// <summary>
-/// Gets the name of the employee.
-/// </summary>
-// TODO: Implement the Name property
-
-/// <summary>
-/// Gets or sets the salary of the employee.
-/// </summary>
-/// <exception cref="ArgumentOutOfRangeException">Thrown value less than 0.</exception>
-// TODO: Implement the Salary property
-
-/// <summary>
-/// Assigns a bonus to the employee.
-/// </summary>
-/// <param name="bonus">The bonus amount.</param>
-/// <exception cref="ArgumentOutOfRangeException">Thrown bonus less than 0.</exception>
-// TODO: Implement the AssignBonus method
-
-/// <summary>
-/// Calculates the total amount to be paid to the employee, including bonuses.
-/// </summary>
-/// <returns>The total amount to be paid to the employee.</returns>
-// TODO: Implement the CalculateTotalPay method
-
-/// <summary>
-/// Returns a string representation of the employee.
-/// </summary>
-/// <returns>A string that represents the employee.</returns>
-// TODO: Implement the ToString method
--- WorkforceStructure/Manager.cs
+++ WorkforceStructure/Manager.cs
@@ -1,32 +1,43 @@
-﻿namespace WorkforceStructure;
+using System.Xml.Linq;
+
+namespace WorkforceStructure;
 
 /// <summary>
 /// Represents a manager, a specialized type of employee.
 /// </summary>
 // TODO: Define the Manager class
+public class Manager : Employee
+{
+    // TODO: Define the private field to store the client count
+    private int clientCount;
 
-// TODO: Define the private field to store the client count
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Manager"/> class with the specified name, salary, and client count.
+    /// </summary>
+    /// <param name="name">The name of the manager.</param>
+    /// <param name="salary">The salary of the manager.</param>
+    /// <param name="clientCount">The number of clients managed by the manager.</param>
+    /// <exception cref="ArgumentNullException">Thrown when name is null.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown salary less than 0.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown clientCount less than 0.</exception>
+    // TODO: Implement the constructor logic
+    public Manager(string name, int salary, int clientCount)
+    : base(name, salary)
+    {
+        this.clientCount = clientCount;
+    }
 
-/// <summary>
-/// Initializes a new instance of the <see cref="Manager"/> class with the specified name, salary, and client count.
-/// </summary>
-/// <param name="name">The name of the manager.</param>
-/// <param name="salary">The salary of the manager.</param>
-/// <param name="clientCount">The number of clients managed by the manager.</param>
-/// <exception cref="ArgumentNullException">Thrown when name is null.</exception>
-/// <exception cref="ArgumentOutOfRangeException">Thrown salary less than 0.</exception>
-/// <exception cref="ArgumentOutOfRangeException">Thrown clientCount less than 0.</exception>
-// TODO: Implement the constructor logic
+    /// <summary>
+    /// Assigns a bonus to the manager based on the number of clients managed.
+    /// </summary>
+    /// <param name="bonus">The base bonus amount.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown bonus less than 0.</exception>
+    // TODO: Implement the AssignBonus method
 
-/// <summary>
-/// Assigns a bonus to the manager based on the number of clients managed.
-/// </summary>
-/// <param name="bonus">The base bonus amount.</param>
-/// <exception cref="ArgumentOutOfRangeException">Thrown bonus less than 0.</exception>
-// TODO: Implement the AssignBonus method
+    /// <summary>
+    /// Returns a string representation of the manager.
+    /// </summary>
+    /// <returns>A string that represents the manager.</returns>
+    // TODO: Implement the ToString method
 
-/// <summary>
-/// Returns a string representation of the manager.
-/// </summary>
-/// <returns>A string that represents the manager.</returns>
-// TODO: Implement the ToString method
+}
