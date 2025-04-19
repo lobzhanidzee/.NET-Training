90dc468d88a348dfc145089fb09489ff602e85a9
lobzhanidzee <daviti.lobzhanidze.1@btu.edu.ge>
2025-04-19 23:55:38 +0400

    fibonacci-iterator 1

--- FibonacciIterator/FibonacciEnumerable.cs
+++ FibonacciIterator/FibonacciEnumerable.cs
@@ -1,12 +1,37 @@
-﻿namespace FibonacciIterator;
+﻿using System.Collections;
+
+namespace FibonacciIterator;
 
 /// <summary>
 /// Represents an enumerable object to iterate over the Fibonacci sequence numbers.
 /// </summary>
-public sealed class FibonacciEnumerable
+/// /// <typeparam name="T">The type of elements in the Fibonacci sequence.</typeparam>
+public sealed class FibonacciEnumerable : IEnumerable<int>
 {
+    private int[] fibonachiList;
+
     // TODO Implement an enumerable to iterate over the Fibonacci sequence numbers.
     public FibonacciEnumerable(int count = int.MaxValue, int skipCount = 0)
     {
+        //ArgumentNullException.ThrowIfNull(fibonachiList);
+
+        //for (int i = skipCount == 0 ? 0 : skipCount - 1; i < count; i++)
+        //{
+
+        //    //yield return (i < 2) ? i : (i - 1) + (i - 2);
+        //}
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        foreach (int number in this.fibonachiList)
+        {
+            yield return number;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
     }
 }
--- FibonacciIterator/FibonacciEnumerator.cs
+++ FibonacciIterator/FibonacciEnumerator.cs
@@ -1,14 +1,48 @@
-﻿namespace FibonacciIterator;
+﻿using System.Collections;
+
+namespace FibonacciIterator;
 
 /// <summary>
 /// Represents an enumerator object to iterate over the Fibonacci sequence numbers.
 /// </summary>
-public sealed class FibonacciEnumerator
+public sealed class FibonacciEnumerator : IEnumerator
 {
+    public FibonacciEnumerable _numbers;
+
+    private int position = -1;
+
+    public object Current => throw new NotImplementedException();
+    //{
+    //    get
+    //    {
+    //        try
+    //        {
+    //            return _numbers;
+    //        }
+    //        catch (IndexOutOfRangeException)
+    //        {
+    //            throw new InvalidOperationException();
+    //        }
+    //    }
+    //}
+
+    object IEnumerator.Current => this.Current;
+
     // TODO Implement an enumerator to iterate over the Fibonacci sequence numbers.
+
     public void Dispose()
     {
         // The method is empty, because there are no additional resources to dispose.
         // See "Notes to Implementers" - https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.ienumerator-1?view=net-6.0#notes-to-implementers
+    }
+
+    public bool MoveNext()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
     }
 }
